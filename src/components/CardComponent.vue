0b7273bec64ae502e974b3d16a02aff8febfee57
Aligern <sasso.luca93@gmail.com>
2024-04-15 18:34:35 +0200

    add api link to cards

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="card my-2 ">
+  <img :src="img" class="card-img-top" :alt="name" >
+  <div class="card-body">
+    <h5 class="card-title">
+        {{ name }}
+    </h5>
+    <p class="card-text">
+        {{ type }}
+    </p>
+  </div>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "CardComponent",
+        props: [
+            'img',
+            'name',
+            'type'
+        ]
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,17 +1,22 @@
 <template>
     <div class="container">
-        <ul>
-            <li v-for="card in store.cards" :key="card.id">
-                {{ card.name }}
-            </li>
-        </ul>
+        <div class="row">
+            <div class="col-12 col-md-4 col-lg-6" v-for="card in store.cards" :key="card.id">
+                <CardComponent 
+                :img="card.card_images[0].image_url" :name="card.name" :type="card.archetype" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import {store} from '../data/store.js'
+import CardComponent from './CardComponent.vue';
     export default {
         name: "CardList",
+        components: {
+            CardComponent
+        },
         data(){
             return {
                 store
